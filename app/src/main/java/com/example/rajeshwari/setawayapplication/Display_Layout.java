6ed3ee2dfce86e4c0866896b4808dfa0484156c9
Rajeshwari11 <rajeshwarikpatel112@gmail.com>
2018-01-25 14:20:02 +0530

    Submitffff

--- app/src/main/java/com/example/rajeshwari/setawayapplication/Display_Layout.java
+++ app/src/main/java/com/example/rajeshwari/setawayapplication/Display_Layout.java
@@ -14,9 +14,6 @@ public class Display_Layout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display_layout);
-
-        //gftujkvcxxtcyvubnmklnbhuvcyrxezwrextcyvh
-
     }
 
 
